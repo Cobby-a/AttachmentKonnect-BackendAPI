d0926f8b90045c14d2d506b35c208431308fa54c
cobby-a <aduboateng333@gmail.com>
2024-07-25 23:15:03 +0000

    modified url for manager role

--- manager/urls.py
+++ manager/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', ManagerList.as_view()),
     path('<str:pk>', ManagerDetail.as_view()),
     path('manager-login/', manager_login),
-    path('roles', RoleDetailList.as_view()),
+    path('roles/', RoleDetailList.as_view()),
     path('roles/<str:pk>/', RoleDetailDetail.as_view()),
     path('companyroles-list/<int:manager_id>/', CompanyRolesList.as_view()),
 ]
